56ab3515c699897298b998d1a8ce41778d3cc8b3
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2017-10-30 22:24:51 +1000

    Rendering IR and depth at the same time.

--- OpenRealSense.Cmd/Program.cs
+++ OpenRealSense.Cmd/Program.cs
@@ -31,40 +31,72 @@ namespace OpenRealSense.Cmd {
             var width = 640;
             var oneMeter = device.GetOneMeterValeForDepth();
 
+            Console.Clear();
+            Console.BufferHeight = Console.WindowHeight = (height / 20 + 1) * 2 + 2;
+            Console.BufferWidth = Console.WindowWidth = width / 10 + 2;
+
+            device.EnableStream(StreamType.Infrared, width, height, FormatType.y16, 30);
             device.EnableStream(StreamType.Depth, width, height, FormatType.Z16, 30);
             device.StartInBackground(() => {
                 Console.CursorVisible = false;
-                var frameInBytes = device.GetFrameData(StreamType.Depth).Bytes;
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+
+                Console.WriteLine("IR:");
+
+                var frameInBytes = device.GetFrameData(StreamType.Infrared).Bytes;
                 using (var stream = new MemoryStream(frameInBytes)) {
-                    using (var reader = new BinaryReader(stream)) {
-                        var buffer = new char[(width / 10 + 1) * (height / 20 + 1)];
-                        var bufferIndex = 0;
-                        var coverage = new int[64];
-                        for (var y = 0; y < height; ++y) {
-                            for (var x = 0; x < width; ++x) {
-                                var depth = reader.ReadUInt16();
-                                if (depth > 0 && depth < oneMeter) {
-                                    ++coverage[x / 10];
-                                }
-                            }
-                            if (y % 20 == 19) {
-                                for (var i = 0; i < coverage.Length; i++) {
-                                    buffer[bufferIndex++] = " .:nhBXWW"[coverage[i] / 25];
-                                    coverage[i] = 0;
-                                }
-                                buffer[bufferIndex++] = '\n';
-                            }
-                        }
-                        buffer[bufferIndex] = ' ';
-                        Console.CursorLeft = 0;
-                        Console.CursorTop = 0;
-                        Console.Write(buffer);
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        Render(height, width, oneMeter, reader);
+                    }
+                }
+
+                Console.CursorLeft = 0;
+                Console.WriteLine("Depth:");
+
+
+                frameInBytes = device.GetFrameData(StreamType.Depth).Bytes;
+                using (var stream = new MemoryStream(frameInBytes))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        Render(height, width, oneMeter, reader);
                     }
                 }
             });
             Console.ReadLine();
             Console.CursorVisible = true;
             device.Stop();
+        }
+
+        private static void Render(int height, int width, float oneMeter, BinaryReader reader)
+        {
+            var buffer = new char[(width / 10 + 1) * (height / 20 + 1)];
+            var bufferIndex = 0;
+            var coverage = new int[64];
+            for (var y = 0; y < height; ++y)
+            {
+                for (var x = 0; x < width; ++x)
+                {
+                    var depth = reader.ReadUInt16();
+                    if (depth > 0 && depth < oneMeter)
+                    {
+                        ++coverage[x / 10];
+                    }
+                }
+                if (y % 20 == 19)
+                {
+                    for (var i = 0; i < coverage.Length; i++)
+                    {
+                        buffer[bufferIndex++] = " .:nhBXWW"[coverage[i] / 25];
+                        coverage[i] = 0;
+                    }
+                    buffer[bufferIndex++] = '\n';
+                }
+            }
+            buffer[bufferIndex] = ' ';
+            Console.Write(buffer);
         }
     }
 }
